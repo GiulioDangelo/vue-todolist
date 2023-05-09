1fc49b8d2e81018afbf834ed5e6a41baf573c6cf
GiulioDangelo <dangelogiulio48@gmail.com>
2023-05-09 19:06:16 +0200

    add bonus

--- script.js
+++ script.js
@@ -1,58 +1,54 @@
 const app = Vue.createApp({
+	data() {
+		return {
+			newTask: "",
 
-    data() {
-        return {
-            newTask : '',
+			error: false,
 
-            error : false,
+			tasks: [
+				{
+					text: "fare la spesa",
+					done: false,
+				},
 
-            tasks : [
-                {
-                  text : 'fare la spesa',
-                  done : false
-                },
-        
-                {
-                  text : "lavare i piatti",
-                  done : true
-                },
-        
-                {
-                  text : 'passare aspirapolvere',
-                  done : false
-                },
-            ],
+				{
+					text: "lavare i piatti",
+					done: true,
+				},
 
+				{
+					text: "passare aspirapolvere",
+					done: false,
+				},
+			],
+		};
+	},
 
-        }
-    },
+	methods: {
+		addTask() {
+			let cleanedTask = this.newTask.trim();
+			if (cleanedTask.length >= 5) {
+				let newObj = {
+					text: cleanedTask,
+					done: false,
+				};
+				this.tasks.unshift(newObj);
+				this.newTask = "";
+				this.error = false;
+			} else {
+				this.error = true;
+			}
+		},
 
+		removeTask(index) {
+			this.tasks.splice(index, 1);
+		},
 
-    methods: {
-        addTask(){
-            let cleanedTask = this.newTask.trim()
-            if(cleanedTask.length >= 4){
-                let newObj = {
-                    text : cleanedTask,
-                    done : false
-                }
-            this.tasks.unshift(newObj);
-            this.newTask = '';
-            this.error = false;
-        }  else {
-            this.error = true;
-        }
-
-    },
-
-    removeTask(index){
-        this.tasks.splice(index,1);
-    },
-
-    // addTextLine(i){
-    //         this.tasks[i].done = !this.tasks[i].done
-    //         // text.addClass('text-decoration-line-through');
-}})
+		doneTodo(i) {
+			this.tasks[i].done = !this.tasks[i].done;
+		},
+	},
+});
 
 
 app.mount('#app');
